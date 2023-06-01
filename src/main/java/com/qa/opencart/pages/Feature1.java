8f2b6e825ce4e42836a1ddc5fbeee4584b524042
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-06-01 07:33:12 +0530

    added feature 1

---
+++ src/main/java/com/qa/opencart/pages/Feature1.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class Feature1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("feature 1");
+
+	
+	}
+
+}
