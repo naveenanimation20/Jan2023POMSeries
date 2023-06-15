b6c1d2ed03d2d3003ef2471ab339fafe259f791c
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-06-15 08:02:31 +0530

    added seleoid aws ip

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -1,5 +1,7 @@
 package com.qa.opencart.factory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -31,6 +33,14 @@ public class OptionsManager {
 			co.setCapability("browserName", "chrome");
 			co.setBrowserVersion(prop.getProperty("browserversion"));
 			//co.setCapability("enableVNC", true);
+//			co.setCapability("se:timeZone", "US/Pacific");
+//			co.setCapability("se:screenResolution", "1920x1080");
+			
+			Map<String, Object> selenoidOptions = new HashMap<>();
+			selenoidOptions.put("screenResolution", "1280x1024x24");
+			selenoidOptions.put("enableVNC", true);
+			selenoidOptions.put("name", prop.getProperty("testname"));
+			co.setCapability("selenoid:options", selenoidOptions);
 		}
 		
 		
